3bb2e983f6b8f76d43e700f09752e13135bf07ae
justanotherbyte <wishymovies@gmail.com>
2025-05-24 00:05:34 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+***
+
+<h1 align="center">
+<sub>
+    <img src=".github/stanford.webp" height="36">
+</sub>
+&nbsp;
+Stanford CS229
+</h1>
+<p align="center">
+<sup>
+Implementations of the theory and models covered in Stanford CS229.
+</sup>
+<br>
+<sup>
+    <a href="https://justanotherbyte.github.io/publications.html">Read the accompanying articles.</a>
+</sup>
+</p>
+
+***
+
+This repository contains implementations of the theory and models covered in [CS229's Autumn 2018](https://www.youtube.com/watch?v=jGwO_UgTS7I&list=PLoROMvodv4rMiGQp3WXShtMGgzqpfVfbU) reading.
+
+Model implementations can be found in their respective categorical folders (supervised, unsupervised, deep, reinforcement). The impl folder contains implementations for common algorithms between the models, such as Gradient Descent.
+
+The way I've chosen to implement these models is **not** indicative of how they're implemented in the real world. Simply use this repository as a reference. Note, some of these models may take a while to train either due to the dataset, bad hyperparameter choices on my part or both. I've added comments where I think this is applicable.
+
+### Articles
+
+I occasionally write accompanying articles for models or theory I find *particularly* interesting. You can find these on my website.
+
+### Dependencies
+
+- NumPy (2.2.6)
+- Matplotlib (3.10.3)
+- Python (3.10+)
+
+### Licensing
+
+This repository is under the MIT License, except from the datasets and .github folders. These directories contain assets that I do not own, hence cannot license.
---
+++ impl/optimization.py
@@ -0,0 +1,78 @@
+from typing import Callable, Union
+
+import numpy as np
+from numpy.typing import NDArray
+from IPython.display import clear_output
+
+
+class GradientDescentAscent:
+    def __init__(
+        self,
+        gradient_function: Callable[[NDArray], Union[NDArray, float]],
+        *,
+        learning_rate: float = 0.01,
+        max_iter: int = 100_000,
+        log_params: bool = False,
+    ):
+        self.gradient = gradient_function
+        self.learning_rate = learning_rate
+        self.max_iter = max_iter
+
+        self._log_params = log_params
+
+    @staticmethod
+    def yield_batches(batch_size: int, *, feature_data: NDArray, label_data: NDArray):
+        while True:
+            for i in range(0, len(feature_data), batch_size):
+                yield (
+                    feature_data[i : i + batch_size],
+                    label_data[i : i + batch_size],
+                )
+
+    def minimize(self, initial: NDArray) -> NDArray:
+        converged = False
+        iterations = 0
+
+        params = initial
+
+        seen = np.array([])
+
+        while (iterations <= self.max_iter) and (converged is False):
+            new = params - (self.learning_rate * self.gradient(params))
+
+            if np.array_equal(params, new):
+                converged = True
+
+            params = new
+            iterations += 1
+
+            if self._log_params:
+                print(params)
+
+        if self._log_params:
+            clear_output(wait=False)
+
+        return params
+
+    def maximize(self, initial: NDArray) -> NDArray:
+        converged = False
+        iterations = 0
+
+        params = initial
+
+        while (iterations <= self.max_iter) and (converged is False):
+            new = params + (self.learning_rate * self.gradient(params))
+
+            if np.array_equal(params, new):
+                converged = True
+
+            params = new
+            iterations += 1
+
+            if self._log_params:
+                print(params)
+
+        if self._log_params:
+            clear_output(wait=False)
+
+        return params
